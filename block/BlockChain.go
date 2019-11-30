027e6c3f2a07320313548ea9eede166d0fb612e3
r000setta <1509248091@qq.com>
2019-11-30 21:43:25 +0800

    Initial commit

---
+++ block/BlockChain.go
@@ -0,0 +1,19 @@
+package block
+
+type Blockchain struct {
+	Blocks []*Block
+}
+
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.Blocks[len(bc.Blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.Blocks = append(bc.Blocks, newBlock)
+}
+
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
+
+func NewBlockChain() *Blockchain {
+	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+import "test/block"
+
+func main() {
+	bc := block.NewBlockChain()
+
+	bc.AddBlock("Send 1 BTC to Ivan")
+	bc.AddBlock("Send 2 more BTC to Ivan")
+
+	for _, block := range bc.Blocks {
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
+}
